94c5b93d89e1fdda4cc11349e8045bd4d133191e
“tkarmstrong” <tkarmstrong@my365.bellevue.edu>
2019-03-17 12:22:43 -0500

    Adding armstrong-exercise-4.3.js

---
+++ week-4/status-codes/armstrong-exercise-4.3.js
@@ -0,0 +1,39 @@
+let express = require("express");
+
+let http = require('http');
+
+let app = express();
+
+app.get("/forbidden", function(req, res) {
+
+  res.status(403);
+
+  res.json({
+      message: "403. There is a charm about the forbidden that makes it unspeakably desireable. -Mark Twain"
+  })
+
+});
+
+app.get("/method", function(req, res) {
+
+  res.status(405);
+
+  res.json({
+      message: "405. I don't dare do anything anymore, 'cause I'm afraid it's not allowed. -Anne Frank"
+  })
+
+});
+
+app.get("/service", function(req, res) {
+
+  res.status(503);
+
+  res.json({
+      message: "503. The truth you believe and cling to makes you unavailable to hear anything new. -Pema Chodron"
+  })
+
+});
+
+http.createServer(app).listen(3000, function() {
+ console.log("Application started on port 3000.");
+});

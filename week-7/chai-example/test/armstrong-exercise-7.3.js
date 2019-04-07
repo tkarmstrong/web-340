6237b401e6313d87d77a32dce9a44baa050fb5ff
“tkarmstrong” <tkarmstrong@my365.bellevue.edu>
2019-04-07 13:12:13 -0500

    Cleaning up.

--- week-7/chai-example/test/armstrong-exercise-7.3.js
+++ week-7/chai-example/test/armstrong-exercise-7.3.js
@@ -15,13 +15,8 @@ var chai = require("chai");
 var assert = chai.assert;
 
 describe("fruits", function() {
-
-    it("should return an array of fruits", function() {
-
-        var f = fruits('Apple,Orange,Mango');
-
-        assert(Array.isArray(f));
-
-    });
-
+  it("should return an array of fruits", function() {
+    var f = fruits('Apple,Orange,Mango');
+    assert(Array.isArray(f));
+  });
 });
